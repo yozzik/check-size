5a3c4375a220558c4397b591beabb57d61e88846
Yehor Levchenko <yozzik@Yehors-MacBook-Pro.local>
2018-09-10 22:29:02 +0300

    spat and polished

--- check_size.pl
+++ check_size.pl
@@ -9,13 +9,13 @@ use strict;
 
 # Main variables
 my $maxSize = 4.7; # Max size in gigabytes
-my $checkSize = $maxSize * (1024 * 1024 * 1024); # Check sum in bytes
+my $maxFileSize = $maxSize * (1024 * 1024 * 1024); # Check sum in bytes
 my $query = "SELECT sum(sizeOfFiles) FROM files;"; # Query to be executed
 my $dsn = "DBI:mysql:luxoft"; # Data Source Name
 my $user = "root";
 my $password = "root";
 
-sub getFilesSizeFromDB {
+sub getSizeOfFilesFromDB {
   # Establish connection, prepare a query, execute it, close connection and return $fileSize
   my $dbh = DBI->connect($dsn, $user, $password, {RaiseError => 1});
   my $sth = $dbh->prepare($query);
@@ -26,17 +26,8 @@ sub getFilesSizeFromDB {
   return ($fileSize);
 }
 
-sub validateFilesSize {
-  # Compare given size with maximum size, return True\False
-  my $size = $_[0];
-  return ($size < $checkSize);
-}
-
-# Execute a query to the DB and get sum of files size
-my $sizeOfFileSum = getFilesSizeFromDB();
-
 # Print verification result based on checkSizeOfFiles subroutine result
-if (validateFilesSize($sizeOfFileSum)) {
+if (getSizeOfFilesFromDB() < $maxFileSize) {
   print "Size of files is less then $maxSize GB.\n";
 }
 else {
